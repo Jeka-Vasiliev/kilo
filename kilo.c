7d27fd6c8ea7febc9e46c9f1dc0f70c7d637889c
Jeka Vasiliev <zheka_v@inbox.ru>
2018-12-08 20:43:52 +0300

    Refactor keyboar input

--- kilo.c
+++ kilo.c
@@ -9,7 +9,7 @@
 
 /*** defines ***/
 
-#define CTRL_KEY(k) ((k) & 0x1f)
+#define CTRL_KEY(k) ((k)&0x1f)
 
 /*** data ***/
 
@@ -42,19 +42,32 @@ void enableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+/*** input ***/
+
+void editorProcessKeyPress() {
+    char c = editorReadKey();
+    switch (c) {
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
+}
+
 /*** init ***/
 
 int main() {
     enableRawMode();
     while (1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if (iscntrl(c)) {
-            printf("%d\n\r", c);
-        } else {
-            printf("%d (%c)\n\r", c, c);
-        }
-        if (c == CTRL_KEY('q')) break;
+        editorProcessKeyPress();
     }
     return 0;
 }
