2b087fafef85912108a1ccab65fb85a5cf713570
Jeka Vasiliev <zheka_v@inbox.ru>
2018-12-10 20:18:05 +0300

    Crear lines 1 at time

--- kilo.c
+++ kilo.c
@@ -117,6 +117,8 @@ void abFree(struct abuf *ab) { free(ab->b); }
 void editorDrawRows(struct abuf *ab) {
     for (int y = 0; y < E.screenrows; y++) {
         abAppend(ab, "~", 1);
+        
+        abAppend(ab, "\x1b[K", 4);
         if (y != E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
         }
@@ -126,7 +128,6 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
     abAppend(&ab, "\x1b[?25l", 6);
-    abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
